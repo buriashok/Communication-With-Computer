553473d446fe7f160df0ab85cc720c5307276984
Ashok kumar <buri.ashokkumar@gmail.com>
2023-09-14 00:22:04 +0530

    insertion in array at given.cpp

---
+++ Program-24.cpp
@@ -0,0 +1,36 @@
+//Insertion of an element at a given postion .
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=5,k,i;
+    int a[n];
+    cout<<"Enter the elements in the array: "<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<"Enter element "<<i<<" : ";
+        cin>>a[i];
+    }
+    cout<<"The array before insertion: ";
+    cout<<"[";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<"]."<<endl;
+    cout<<"Enter the Position where element should insert: ";
+    cin>>k;
+    for(i=n;i<=k;i--)
+    {
+        a[n+1]=a[n];
+    }
+    cout<<"Enter the element should be inserted: ";
+    cin>>a[k];
+    cout<<"[";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<"].";
+    return 0;
+}
